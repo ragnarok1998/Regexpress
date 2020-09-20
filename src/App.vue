72461992c3296ffb2a6206ccb2475164107bcf4a
gouravchandna <gouravchandna1998@gmail.com>
2020-09-21 02:27:59 +0530

    initial commit boilerpalte,tailwind added

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    
+    <HelloWorld msg="Regexpress"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+<style src="./assets/tailwind.css"></style>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
